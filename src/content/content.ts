e2b82d4eee783209394942b2f704ba34dfecd8cc
Abhishek <abhishekY495.sec@gmail.com>
2024-11-09 15:30:52 +0530

    refactor

--- src/content/content.ts
+++ src/content/content.ts
@@ -4,7 +4,7 @@ import { checkIfVideoIsInLikedDB } from "./indexedDB/checkIfVideoIsInLikedDB";
 import { getVideoUrlSlug } from "../helpers/getVideoUrlSlug";
 
 const urlSlug = getVideoUrlSlug();
-await checkIfVideoIsInLikedDB(String(urlSlug));
+checkIfVideoIsInLikedDB(String(urlSlug));
 
 let lastUrl = location.href;
 new MutationObserver(async () => {
@@ -15,27 +15,3 @@ new MutationObserver(async () => {
     await checkIfVideoIsInLikedDB(String(urlSlug));
   }
 }).observe(document, { subtree: true, childList: true });
-
-// if (document.readyState === "complete") {
-//   let observer = new MutationObserver((mutations, observer) => {
-//     const actionsElement = document.querySelector("#actions-inner");
-//     // if (!actionsElement) return;
-
-//     if (actionsElement) {
-//       const likeBtn = actionsElement.querySelector(
-//         ".YtLikeButtonViewModelHost"
-//       );
-//       if (likeBtn) {
-//         console.log(likeBtn);
-//         observer.disconnect();
-//       }
-//     }
-
-//     console.log("observing");
-//   });
-
-//   observer.observe(document.body, {
-//     childList: true,
-//     subtree: true,
-//   });
-// }
