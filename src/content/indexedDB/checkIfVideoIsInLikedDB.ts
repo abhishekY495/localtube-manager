8741f07557957b7ab308cce3737b4d3038685f03
Abhishek <abhishekY495.sec@gmail.com>
2024-11-13 07:38:50 +0530

    fix: like/unlike video and styling

--- src/content/indexedDB/checkIfVideoIsInLikedDB.ts
+++ src/content/indexedDB/checkIfVideoIsInLikedDB.ts
@@ -1,6 +1,6 @@
 import { clickTitle } from "../../helpers/clickTitle";
 import { getVideoObj } from "../../helpers/getVideoObj";
-import { likedIcon } from "../../helpers/likedUnlikedIcons";
+import { likedIcon, notLikedIcon } from "../../helpers/likedUnlikedIcons";
 import { initializeYoutubeDB } from "./initializeYoutubeDB";
 import { toggleLikeVideo } from "./toggleLikeVideo";
 
@@ -9,7 +9,6 @@ let observer: MutationObserver | null = null;
 export async function checkIfVideoIsInLikedDB(urlSlug: string) {
   const db = await initializeYoutubeDB();
   const video = await db.get("likedVideos", urlSlug);
-
   console.log(video ? "Video liked" : "Video not liked");
 
   if (observer) {
@@ -17,53 +16,73 @@ export async function checkIfVideoIsInLikedDB(urlSlug: string) {
     observer = null;
   }
 
-  // Initialize the observer
-  observer = new MutationObserver(() => {
-    const actionsElement = document.querySelector("#actions-inner");
-    if (!actionsElement) return;
-
-    const likeBtn = actionsElement.querySelector(".YtLikeButtonViewModelHost");
-    if (!likeBtn) return;
-
-    const iconElement = likeBtn.querySelector(
-      ".yt-spec-button-shape-next__icon"
-    );
-    if (!iconElement) return;
-
-    // Retry mechanism to ensure YouTube has fully rendered the icon element
-    const applyIconUpdate = () => {
-      if (iconElement.innerHTML !== likedIcon) {
-        iconElement.innerHTML = likedIcon;
-
-        // Disconnect the observer if the icon has been updated and listener added
-        if (
-          likeBtn.hasAttribute("listenerAdded") &&
-          iconElement.innerHTML === likedIcon
-        ) {
-          observer?.disconnect();
-        }
+  setTimeout(() => {
+    observer = new MutationObserver(() => {
+      const aboveTheFoldElement = document.querySelector(
+        "#above-the-fold"
+      ) as HTMLElement;
+      if (aboveTheFoldElement === null) {
+        console.log("aboveTheFoldElement not found");
+        return;
       }
-    };
+      console.log(aboveTheFoldElement);
 
-    // Add click event listener if not already added
-    if (!likeBtn.hasAttribute("listenerAdded")) {
-      likeBtn.setAttribute("listenerAdded", "true");
+      const YtdMenuRenderer = aboveTheFoldElement.querySelector(
+        "ytd-menu-renderer"
+      ) as HTMLElement;
+      if (YtdMenuRenderer === null) {
+        console.log("YtdMenuRenderer not found");
+        return;
+      }
+      console.log(YtdMenuRenderer);
+
+      const likeButtonViewModelHost = YtdMenuRenderer.querySelector(
+        "like-button-view-model"
+      ) as HTMLElement;
+      if (likeButtonViewModelHost === null) {
+        console.log("likeButtonViewModelHost not found");
+        return;
+      }
+      console.log(likeButtonViewModelHost);
+
+      const likeCountElement = document.querySelector(
+        '[aria-label*="likes"]'
+      ) as HTMLElement;
+      if (likeCountElement === null) {
+        console.log("likeCountElement not found");
+        return;
+      }
+      const likeCount = likeCountElement.querySelector(
+        ".YtwFactoidRendererValue"
+      ) as HTMLElement;
+      if (likeCount === null) {
+        console.log("likeCount not found");
+        return;
+      }
+      console.log(likeCount?.innerText);
+
+      likeButtonViewModelHost.innerHTML = `
+      <div id="custom-nologin-yt-like-btn">
+        <div id="custom-nologin-yt-like-btn-icon" data-custom-no-login-yt-btn-icon-liked=${
+          video ? "initial-liked" : "initial-not-liked"
+        }>${video ? likedIcon : notLikedIcon}</div>
+        <div id="custom-nologin-yt-like-count">
+        ${likeCount?.innerText}
+        </div>
+      </div>
+      `;
+
+      const likeBtn = document.querySelector(
+        "#custom-nologin-yt-like-btn"
+      ) as HTMLElement;
       likeBtn.addEventListener("click", async () => {
         const video = getVideoObj(document);
         await toggleLikeVideo(video, likeBtn);
         clickTitle();
       });
-    }
 
-    // Delay execution to avoid conflicts with YouTube’s rendering
-    setTimeout(() => {
-      if (video) {
-        applyIconUpdate();
-      } else {
-        observer?.disconnect();
-      }
-    }, 1500);
-  });
-
-  observer.observe(document.body, { childList: true, subtree: true });
+      observer?.disconnect();
+    });
+    observer.observe(document.body, { childList: true, subtree: true });
+  }, 1500);
 }
--- src/content/indexedDB/toggleLikeVideo.ts
+++ src/content/indexedDB/toggleLikeVideo.ts
@@ -7,24 +7,32 @@ export async function toggleLikeVideo(video: Video, likeBtn: Element) {
   const db = await initializeYoutubeDB();
   const tx = db.transaction("likedVideos", "readwrite");
   const likedVideosStore = tx.objectStore("likedVideos");
-  const iconElement = likeBtn.querySelector(".yt-spec-button-shape-next__icon");
+  const iconElement = likeBtn.querySelector("#custom-nologin-yt-like-btn-icon");
 
   try {
-    const existingVideo = await likedVideosStore.get(video.urlSlug);
+    const existingVideo: Video = await likedVideosStore.get(video.urlSlug);
     if (existingVideo) {
       await likedVideosStore.delete(video.urlSlug);
       // unlike icon
+      iconElement?.setAttribute(
+        "data-custom-no-login-yt-btn-icon-liked",
+        "not-liked"
+      );
       if (iconElement) {
         iconElement.innerHTML = notLikedIcon;
       }
-      console.log("Video removed from liked videos:", existingVideo);
+      console.log("Video removed from liked videos:", existingVideo.title);
     } else {
       await likedVideosStore.add(video);
+      iconElement?.setAttribute(
+        "data-custom-no-login-yt-btn-icon-liked",
+        "liked"
+      );
       // like animation
       if (iconElement) {
-        iconElement.innerHTML = `<canvas id="dotlottie-canvas" style="width: 58px; height: 58px; margin-top:-16px; margin-left:-16px"></canvas>`;
+        iconElement.innerHTML = `<canvas id="custom-nologin-yt-dotlottie-canvas" style="width: 58px; height: 58px; margin-top:-16px; margin-left:-16px; margin-right:-18px;"></canvas>`;
         const canvasElement = document.querySelector(
-          "#dotlottie-canvas"
+          "#custom-nologin-yt-dotlottie-canvas"
         ) as HTMLCanvasElement;
         new DotLottie({
           autoplay: true,
