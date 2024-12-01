88fbf7850e80838a5a1876f3e52427264a3af421
Abhishek <abhishekY495.sec@gmail.com>
2024-12-01 18:51:34 +0530

    refactor

--- src/helpers/video/clickTitle.ts
+++
@@ -1,8 +0,0 @@
-export function clickTitle() {
-  const videoTitle = document.querySelector(
-    "yt-formatted-string"
-  ) as HTMLElement;
-  if (videoTitle) {
-    videoTitle.click();
-  }
-}
--- src/helpers/video/removePopupModalContainer.ts
+++
@@ -1,9 +0,0 @@
-export function removePopupModalContainer() {
-  const popupContainer = document.querySelector(
-    "ytd-modal-with-title-and-button-renderer"
-  );
-  if (popupContainer) {
-    popupContainer.remove();
-    console.log("Popup container removed");
-  }
-}
