214189b868505c1ceeed7d2a617fe3b5b2b02df4
Abhishek <abhishekY495.sec@gmail.com>
2026-04-23 19:30:25 +0530

    chore: add Tailwind CSS and Vite integration, remove unused assets, and clean up styles

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,35 +1,7 @@
-import { useState } from 'react';
-import reactLogo from '@/assets/react.svg';
-import wxtLogo from '/wxt.svg';
-import './App.css';
-
-function App() {
-  const [count, setCount] = useState(0);
-
+export const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://wxt.dev" target="_blank">
-          <img src={wxtLogo} className="logo" alt="WXT logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>WXT + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the WXT and React logos to learn more
-      </p>
-    </>
+    <div className="size-64 bg-neutral-900 text-white p-2">
+      <h1 className="text-2xl font-bold text-center">LocalTube Manager</h1>
+    </div>
   );
-}
-
-export default App;
+};
