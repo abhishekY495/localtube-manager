733d5fdfed0d5256468e0267921027af0f82863b
Abhishek <abhishekY495.sec@gmail.com>
2026-04-23 20:37:56 +0530

    refactor and add manifest details and permissions

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,7 +4,15 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ["@wxt-dev/module-react"],
+  srcDir: "src",
   vite: () => ({
     plugins: [tailwindcss()],
   }),
+  manifest: {
+    name: "LocalTube-Manager",
+    description: "A browser extension to use Youtube without a Google account",
+    version: "5.0.0",
+    permissions: ["unlimitedStorage", "alarms", "notifications"],
+    host_permissions: ["*://*.youtube.com/*"],
+  },
 });
