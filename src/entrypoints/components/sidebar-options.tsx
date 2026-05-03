e75823e4335136116880e8828d890db707a5764f
Abhishek <abhishekY495.sec@gmail.com>
2026-05-03 19:51:42 +0530

    refactor: remove unused icon and tooltip from sidebar options for cleaner UI

--- src/entrypoints/components/sidebar-options.tsx
+++ src/entrypoints/components/sidebar-options.tsx
@@ -1,5 +1,4 @@
-import { RotateCwIcon, SquareArrowOutUpRightIcon, XIcon } from "lucide-react";
-import { ACTIONS } from "../utils/constants";
+import { RotateCwIcon, XIcon } from "lucide-react";
 
 export const SidebarOptions = ({
   setIsOpen,
@@ -59,33 +58,9 @@ export const SidebarOptions = ({
           Refresh
         </span>
         <RotateCwIcon
-          size={15}
+          size={16}
           className="cursor-pointer"
           onClick={onRefresh}
-        />
-      </div>
-      <div className="group relative inline-flex">
-        <span
-          className="pointer-events-none absolute z-50 whitespace-nowrap rounded border border-neutral-600 bg-neutral-800 opacity-0 shadow transition-opacity delay-0 duration-200 group-hover:opacity-100 group-hover:delay-500"
-          role="tooltip"
-          style={{
-            marginTop: "4px",
-            paddingInline: "8px",
-            paddingBlock: "4px",
-            fontSize: "12px",
-            top: "-245%",
-            left: "50%",
-            translate: "-50%",
-          }}
-        >
-          Open in new tab
-        </span>
-        <SquareArrowOutUpRightIcon
-          size={14}
-          className="cursor-pointer"
-          onClick={() =>
-            browser.runtime.sendMessage({ action: ACTIONS.OPEN_DASHBOARD })
-          }
         />
       </div>
     </div>
