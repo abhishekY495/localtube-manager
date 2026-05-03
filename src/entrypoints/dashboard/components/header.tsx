fb967dac0b9718f679465b9b9d822375f07e9220
Abhishek <abhishekY495.sec@gmail.com>
2026-05-03 19:00:51 +0530

    refactor: replace anchor tag with div in header component for improved structure

--- src/entrypoints/dashboard/components/header.tsx
+++ src/entrypoints/dashboard/components/header.tsx
@@ -1,12 +1,9 @@
 export const Header = () => {
   return (
-    <a
-      href="/dashboard.html"
-      className="bg-neutral-900 flex items-center justify-center pb-3 pt-2 px-6 rounded w-fit mx-auto cursor-pointer"
-    >
+    <div className="bg-neutral-900 flex items-center justify-center pb-3 pt-2 px-6 rounded w-fit mx-auto cursor-pointer">
       <h1 className="text-center text-xl font-semibold text-neutral-200">
         LocalTube Manager
       </h1>
-    </a>
+    </div>
   );
 };
