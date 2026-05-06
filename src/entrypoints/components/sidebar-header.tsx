ffcaf4723c47e150d71a5f7255cbab14d2c91a36
Abhishek <abhishekY495.sec@gmail.com>
2026-05-06 18:07:21 +0530

    styling

--- src/entrypoints/components/sidebar-header.tsx
+++ src/entrypoints/components/sidebar-header.tsx
@@ -1,13 +1,21 @@
+import logo from "@/assets/logo.png";
+
 export const SidebarHeader = () => {
   return (
     <header
-      className="border-b border-neutral-700 bg-neutral-800 font-medium text-center"
+      className="border-b border-neutral-700 bg-neutral-800 flex items-center gap-3 justify-center"
       style={{
         paddingBlock: "8px",
-        fontSize: "18px",
       }}
     >
-      LocalTube Manager
+      <img
+        src={logo}
+        alt="LocalTube Manager"
+        className="rounded"
+        width={16}
+        height={16}
+      />
+      <p style={{ fontSize: "18px", fontWeight: 500 }}>LocalTube Manager</p>
     </header>
   );
 };
