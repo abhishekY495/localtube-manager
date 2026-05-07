edb323b95eac633d6641f92b0c9564f9088ba0f7
Abhishek <abhishekY495.sec@gmail.com>
2026-05-07 11:18:53 +0530

    styling

--- src/entrypoints/dashboard/components/header.tsx
+++ src/entrypoints/dashboard/components/header.tsx
@@ -1,9 +1,24 @@
+import logo from "@/assets/logo.png";
+import { WEBSITE_URL } from "@/entrypoints/utils/constants";
+
 export const Header = () => {
   return (
     <div className="bg-neutral-900 flex items-center justify-center pb-3 pt-2 px-6 rounded w-fit mx-auto cursor-pointer">
-      <h1 className="text-center text-xl font-semibold text-neutral-200">
-        LocalTube Manager
-      </h1>
+      <a href={WEBSITE_URL} target="_blank" className="flex items-center gap-4">
+        <img
+          src={logo}
+          alt="LocalTube Manager"
+          className="rounded"
+          width={18}
+          height={18}
+        />
+        <p
+          className="text-neutral-300"
+          style={{ fontSize: "20px", fontWeight: 500 }}
+        >
+          LocalTube Manager
+        </p>
+      </a>
     </div>
   );
 };
