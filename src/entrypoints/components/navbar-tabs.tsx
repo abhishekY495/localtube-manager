b790ecc63c276d1632a8b84ae9e14d3005a13824
Abhishek <abhishekY495.sec@gmail.com>
2026-05-14 21:47:24 +0530

    refactor

--- src/entrypoints/components/navbar-tabs.tsx
+++ src/entrypoints/components/navbar-tabs.tsx
@@ -1,29 +1,8 @@
-import { ACTIONS, NAV_ITEMS, NAV_ITEM_LABELS } from "../utils/constants";
+import { ACTIONS, NAV_ITEMS } from "../utils/constants";
 import type { NavItemLabel } from "../utils/constants";
 import { formatNumber } from "../utils/format-number";
+import { getNavItemCount } from "../utils/get-nav-item-count";
 import type { CountResponse, Message, Response } from "../utils/types";
-
-const getNavItemCount = (
-  label: NavItemLabel,
-  count: CountResponse | null,
-): number | string => {
-  if (!count) {
-    return "";
-  }
-
-  switch (label) {
-    case NAV_ITEM_LABELS.SUBSCRIPTIONS:
-      return count.subscriptionsCount;
-    case NAV_ITEM_LABELS.LIKED_VIDEOS:
-      return count.likedVideosCount;
-    case NAV_ITEM_LABELS.CHANNELS:
-      return count.subscribedChannelsCount;
-    case NAV_ITEM_LABELS.PLAYLISTS:
-      return count.youtubePlaylistsCount + count.localPlaylistsCount;
-    default:
-      return "";
-  }
-};
 
 export const NavbarTabs = ({
   setActiveItem,
