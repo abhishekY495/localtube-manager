267a4e4ccb10c16008c5b4c0bde2553a44247821
Abhishek <abhishekY495.sec@gmail.com>
2026-05-18 16:35:25 +0530

    chore: update version to 5.1.1

--- wxt.config.ts
+++ wxt.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   manifest: ({ browser }) => ({
     name: "LocalTube Manager",
     description: "A browser extension to use YouTube without a Google account",
-    version: "5.1.0",
+    version: "5.1.1",
     action: {},
     permissions:
       browser === "firefox"
